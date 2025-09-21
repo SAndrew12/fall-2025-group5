060fb69f97fe18f329f452ae8ae59fd4217c0895
SAndrew12 <109983877+SAndrew12@users.noreply.github.com>
2025-09-21 11:57:02 -0400

    dynamic data loading

---
+++ src/data_loader.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import os
+
+def load_data():
+    filename = 'ACLED Data_2025-09-11.csv'
+    base_dir = os.path.dirname(os.path.dirname(__file__))
+    data_path = os.path.join(base_dir, "src", "data", filename)
+    df = pd.read_csv(data_path)
+
+    return df
