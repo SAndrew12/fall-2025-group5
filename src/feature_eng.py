c472e960e7bf0fadc2d515dcbe764ee3284911e3
SAndrew12 <109983877+SAndrew12@users.noreply.github.com>
2025-09-21 20:00:43 -0400

    more updates -- needs more work, dataloading issues

--- src/feature_eng.py
+++ src/feature_eng.py
@@ -65,13 +65,72 @@ violence_against_women_tags = [
 #-----Tags for 'violence against women tags'----
 
 #----Load Data---
-df = load_data()
+# df = load_data()
+# print(df.shape)
 #----Load Data---
 
 #---------Feature Eng--------
 
 
 def feature_creating(df):
+    # -----Cols to Drop----
+    to_drop = [
+        'event_id_cnty',
+        'disorder_type',
+        'time_precision',
+        'source',
+        'source_scale',
+        'iso',
+        'country',
+        'timestamp',
+        'geo_precision',
+        'year',
+        'region',
+        'latitude',
+        'longitude',
+        'interaction']
+    # -----Cols to Drop----
+
+    # ------Groups-----
+    unattrib = [
+        'Unidentified Armed Group (Afghanistan)',
+        'Taliban and/or Islamic State Khorasan Province (ISKP)'
+
+    ]
+
+    taliban = [
+        'Taliban',
+        'Taliban - Red Unit',
+        'Mutiny of Taliban'
+    ]
+
+    iskp = [
+        'Islamic State Khorasan Province (ISKP)'
+    ]
+
+    other = [
+        'Al Qaeda',
+        'HQN: Haqqani Network',
+        'TTP: Tehreek-i-Taliban Pakistan',
+        'LeI: Lashkar-e-Islam'
+    ]
+    # ------Groups-----
+
+    # -----Tags for 'violence against women tags'----
+    violence_against_women_tags = [
+        'women targeted: government officials',
+        'women targeted: girls',
+        'women targeted: girls; women targeted: relatives of targeted groups or persons',
+        'local administrators',
+        'women targeted: government officials; women targeted: relatives of targeted groups or persons',
+        'women targeted: candidates for office',
+        'women targeted: activists/human rights defenders/social leaders',
+        'women targeted: relatives of targeted groups or persons',
+        'local administrators; women targeted: politicians',
+        'women targeted: activists/human rights defenders/social leaders; women targeted: government officials'
+    ]
+    # -----Tags for 'violence against women tags'----
+
     df = df.drop(columns=to_drop)
     df['event_date'] = pd.to_datetime(df['event_date'])
     df = df.sort_values(by='event_date', ascending=True)
@@ -113,7 +172,10 @@ def feature_creating(df):
 
 
 
-    return working_df,  unattrib_df
+    return working_df
 
 #---------Feature Eng--------
 
+# working_df  = feature_creating(df)
+#
+# print(working_df.shape)
--- src/main.py
+++ src/main.py
@@ -1,18 +1,25 @@
 import pandas as pd
 from data_loader import load_data
 from feature_eng import feature_creating
-#from classical_models import
+from classical_models import t_t_s
 
 #Load data
 df = load_data()
 
 #Pre-proccess data + features
-working_df , unattrib_df = feature_creating(df)
+working_df  = feature_creating(df)
 print(working_df.shape)
-print(unattrib_df.shape)
+
 #looks good
 
+
+print(working_df['target'].value_counts())
+print(working_df['target'].unique())
 #Classical models
 
-
-
+# X_train, X_test, y_train, y_test = t_t_s(working_df)
+#
+# print(X_train.shape)
+# print(X_test.shape)
+# print(y_train.shape)
+# print(y_test.shape)
