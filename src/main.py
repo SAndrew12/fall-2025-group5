dff98c04eb4a81ab11aa6fc16dee10e031d25be0
SAndrew12 <109983877+SAndrew12@users.noreply.github.com>
2025-11-08 20:57:26 -0500

    turned bert on for exai

--- src/main.py
+++ src/main.py
@@ -18,8 +18,8 @@ from xai_explanations import (
 # ============================================================================
 # CONFIGURATION: Choose what to run
 # ============================================================================
-RUN_CLASSICAL = True
-RUN_BERT = False
+RUN_CLASSICAL = False
+RUN_BERT = True
 RUN_FEATURE_FUSION = False
 
 # XAI CONFIGURATION
