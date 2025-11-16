dc84a241ad05b089fee85f87fb1da355d8398908
SAndrew12 <109983877+SAndrew12@users.noreply.github.com>
2025-11-16 15:39:19 -0500

    ff results

--- src/main.py
+++ src/main.py
@@ -348,6 +348,7 @@ def run_feature_fusion():
 
     y = working_df['target']
 
+
     # 4. Train-test split
     from sklearn.model_selection import train_test_split
     X_text_train, X_text_test, X_man_train, X_man_test, y_train, y_test = train_test_split(
